e0b6ad2d8ee835dd50de485826fda700e76d8b56
artemy <artemy.rusinov@urfu.me>
2023-10-15 19:05:05 +0500

    initial commit

---
+++ src/timus/task_1293/Main.java
@@ -0,0 +1,14 @@
+package timus.task_1293;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] input = in.nextLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int a = Integer.parseInt(input[1]);
+        int b = Integer.parseInt(input[2]);
+        System.out.println(2 * n * a * b);
+    }
+}
